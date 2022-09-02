c99d06d121851d6f8dfa94b153012f86186c7f68
jeroen-talk <jeroen.arians@gmail.com>
2022-09-02 16:30:39 +0200

    add_vectors tussentijdse opslag

--- scripts/practicum_lin_algebra/add_vectors.py
+++ scripts/practicum_lin_algebra/add_vectors.py
@@ -5,12 +5,30 @@
 a = [0, 3, 1]
 b = [1, 4, 7]
 
+#vectoren landen in nieuwe lijst en dan op tellen
 c= [a[0]+b[0],a[1]+b[1],a[2]+b[2] ]
 
 print(c)
 
+aLabel = [0, 3, 1]
+bLabel = [1, 4, 7]
 
-# Matrix Sample
+# funtie
+def addVectors(aLabel, bLabel)
+# voor elke rij
+ for:aLabel in bLabel:
+     aLabel.append(bLabel)
+         #tel de cooresponderende elementen bij elkaar op
+
+    #return resultaat
+
+v1 = list [1, 2]
+v2 = list [2, 1]
+
+w = addVectors(v1, v2)
+
+
+# Voorbeelden van Matrix berekingen
 #
 # A = [[1, 2, 3],
 #      [4, 5, 6],
