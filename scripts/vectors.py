aeb77e62ee6da1bf68e8f7b57f2686125e9ca614
jeroen-talk <jeroen.arians@gmail.com>
2022-09-06 15:05:32 +0200

    Opdracht Tuple Af

---
+++ scripts/vectors.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+ This is a class to generate vectors
+"""
+
+
+def _int_(self, scalerList):
+    self.scalerList = scalerList
+
+
+def addVector(self, otherVector):
+    pass
+
+
+def multiplyVector(self, otherVerctor):
+    pass
+
+
+def printVector(self):
+    print(f"Dit is de vector: {self.scaleList}")
+
+
+list = [9, 8]
+v = Vector(list)
+v.printVector()
