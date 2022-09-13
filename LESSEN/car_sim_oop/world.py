bf7b0f075d4d2605c6a9eab4796a8bd534cce825
jeroen-talk <jeroen.arians@gmail.com>
2022-09-13 13:21:47 +0200

    Sets Af

--- LESSEN/car_sim_oop/world.py
+++ LESSEN/car_sim_oop/world.py
@@ -7,20 +7,25 @@ class World:
     def __init__(self):
         self.t = 0.0
         self.dt = 0.1
-        self.carA = Car("BMW", 600.0, 800.0)
-        self.carB = Car("Tesla", 800.0, 800.0)
+
+        self.running = True
+
+    def
+    addCar(self, car):
+        self.cars.append(car)
 
     def bang(self):
         while self.running:
             self.t += self.dt
-
-            self.carA.move(self.dt)
-            self.carB.move(self.dt)
-
-            if self.carA.x > 100:
+            self.car.move(self.dt)
+            if self.car.x > 100:
                 self.running = False
-            if self.carB.x > 100:
-                self.running = False
-
             print("Time: ", self.t)
             tm.sleep(self.dt)
+
+    def stop(self):
+        self.running = False
+
+
+BMW = Car("BMW", 800, 700)
+Tesla = Car("Tesla", 900, 600)
