659f3776ace1e6d74ad0128958e8c58f5d2e1a8a
Jeroen-talk <jeroen.arians@gmail.com>
2022-09-28 10:22:08 +0200

    TensorFlow

--- simpylc/start_world.sh
+++ simpylc/start_world.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-# export LIBGL_ALLOW_SOFTWARE=1
-python3 simpylc/simulations/car/world.py    
+export LIBGL_ALLOW_SOFTWARE=1
+# python simpylc/simulations/car/world.py    
+python /simulations/car/world.py
---
+++ simpylc/tensorflow_sample_setup/.ipynb_checkpoints/tensorflow_setup-checkpoint.py
@@ -0,0 +1,17 @@
+import pandas as pd
+print(pd.__version__)
+
+'''import numpy as np
+
+# Make numpy values easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+abalone_train = pd.read_csv(
+    "https://storage.googleapis.com/download.tensorflow.org/data/abalone_train.csv",
+    names=["Length", "Diameter", "Height", "Whole weight", "Shucked weight",
+           "Viscera weight", "Shell weight", "Age"])
+
+abalone_train.head()'''
